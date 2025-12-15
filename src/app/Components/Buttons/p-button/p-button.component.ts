54db4bfd1f4076cd033d8378f472b6e2bb2663bd
renatoalvesjr <renato.alvesjr00@gmail.com>
2025-12-15 11:47:29 -0300

    fix: botao de criar campanha

--- src/app/Components/Buttons/p-button/p-button.component.ts
+++ src/app/Components/Buttons/p-button/p-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Attribute, Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'p-button',
@@ -10,4 +10,24 @@ import { Component, Input } from '@angular/core';
 export class PButtonComponent {
   @Input() title: string = '';
   @Input() disabled: boolean = false;
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() autofocus: boolean = false;
+  @Input() ariaLabel: string = '';
+  @Output() buttonClick = new EventEmitter<MouseEvent>();
+  @Output() buttonBlur = new EventEmitter<FocusEvent>();
+  @Output() buttonFocus = new EventEmitter<FocusEvent>();
+
+  onClick(event: MouseEvent): void {
+    if (!this.disabled) {
+      this.buttonClick.emit(event);
+    }
+  }
+
+  onBlur(event: FocusEvent): void {
+    this.buttonBlur.emit(event);
+  }
+
+  onFocus(event: FocusEvent): void {
+    this.buttonFocus.emit(event);
+  }
 }
