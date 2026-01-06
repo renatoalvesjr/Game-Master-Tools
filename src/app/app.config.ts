4596e7042c6cfcd15709a39a69ac12f824276a96
renatoalvesjr <grangamin@gmail.com>
2026-01-06 14:51:02 -0300

    ffoorrmmaatt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,16 @@ import {
   ApplicationConfig, importProvidersFrom, provideAppInitializer,
   provideZoneChangeDetection
 } from '@angular/core';
-import {provideRouter, withRouterConfig} from '@angular/router';
+import { provideRouter, withRouterConfig } from '@angular/router';
 
-import {routes} from './app.routes';
-import {provideAnimations} from '@angular/platform-browser/animations';
-import {initializeApp} from './appInitializer';
-import {CampaignService} from './Services/campaign.service';
-import {HttpClient, provideHttpClient} from '@angular/common/http';
-import {provideAngularSvgIcon} from 'angular-svg-icon';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp } from './appInitializer';
+import { CampaignService } from './Services/campaign.service';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideAngularSvgIcon } from 'angular-svg-icon';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
   new TranslateHttpLoader(http, './i18n/', '.json');
@@ -32,10 +32,10 @@ export const appConfig: ApplicationConfig = {
     CampaignService,
     provideAppInitializer(initializeApp),
     provideRouter(routes,
-      withRouterConfig({onSameUrlNavigation: 'reload'}),
-      withRouterConfig({paramsInheritanceStrategy: 'always'}),
-      withRouterConfig({urlUpdateStrategy: 'eager'}),
-      withRouterConfig({defaultQueryParamsHandling: 'merge'}),
+      withRouterConfig({ onSameUrlNavigation: 'reload' }),
+      withRouterConfig({ paramsInheritanceStrategy: 'always' }),
+      withRouterConfig({ urlUpdateStrategy: 'eager' }),
+      withRouterConfig({ defaultQueryParamsHandling: 'merge' }),
     ),
   ],
 };
